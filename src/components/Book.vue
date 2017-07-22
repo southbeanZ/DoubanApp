16cf78efeeb01587dc0d0b84467b32d8630a8d73
Zero Zhang <southbean2013@gmail.com>
2017-07-23 00:34:00 +0800

    add Book component

---
+++ src/components/Book.vue
@@ -0,0 +1,88 @@
+<template>
+	<div class="m-book">
+		<a href="" class="u-link">
+			<img :src="pic" alt="" class="pic">
+		</a>
+		<p class="u-name">{{name}}</p>
+		<div class="m-score">
+			<div class="u-star">
+				<span class="start" :class="{'none': score == 0,'half': score <= 1.5}"></span>
+				<span class="start" :class="{'none': score <= 2.0,'half': score <= 3.5}"></span>
+				<span class="start" :class="{'none': score <= 4.0,'half': score <= 5.5}"></span>
+				<span class="start" :class="{'none': score <= 6.0,'half': score <= 7.5}"></span>
+				<span class="start" :class="{'none': score <= 8.0,'half': score <= 9.5}"></span>
+			</div>
+			<span class="u-num">{{score}}</span>
+		</div>
+	</div>
+</template>
+<style lang="scss">
+.m-book {
+	display: block;
+	width: 100px;
+	// height: 147px;
+	.u-link {
+		display: block;
+		width: 100%;
+		height: 100%;
+		.pic {
+			width: 100%;
+			height: 100%;
+		}
+	}
+	.u-name {
+		font-size: 14px;
+		line-height: 20px;
+		color: #494949;
+	}
+	.m-score {
+		width: 100%;
+		height: 20px;
+		.u-star {
+			display: inline-block;
+			height: 100%;
+			vertical-align: top;
+			font-size: 0;
+			.start {
+				display: inline-block;
+				width: 13px;
+				height: 100%;
+				background-size: 13px 13px;
+				background-position: 50% 50%;
+				background-repeat: no-repeat;
+				background-image: url(../assets/images/rating_star_xsmall_on.png);
+				&.half {
+					background-image: url(../assets/images/rating_star_xsmall_half.png);
+				}
+				&.none {
+					background-image: url(../assets/images/rating_star_xsmall_off.png);
+				}
+			}
+		}
+		.u-num {
+			font-size: 14px;
+			line-height: 20px;
+			vertical-align: top;
+			color: #ccc;
+		}
+	}
+}
+</style>
+<script>
+export default {
+	props: {
+		pic: {
+			type: String,
+			default: ""
+		},
+		name: {
+			type: String,
+			default: ""
+		},
+		score: {
+			type: Number,
+			default: ""
+		},
+	}
+}
+</script>
--- src/pages/AudioBook/AudioBook.vue
+++ src/pages/AudioBook/AudioBook.vue
@@ -8,16 +8,22 @@
       	<img src="../../assets/images/ic_actionbar_search_icon.png" class="icon">
       </a>
     </Topbar>
-    书影音
+    <Book pic="https://img3.doubanio.com/lpic/s23128183.jpg" name="杀死一只知更鸟" :score="8.0"></Book>
+    <Book pic="https://img3.doubanio.com/lpic/s23128183.jpg" name="22" :score="3.4"></Book>
+    <Book pic="https://img3.doubanio.com/lpic/s23128183.jpg" name="11" :score="1.6"></Book>
+    <Book pic="https://img3.doubanio.com/lpic/s23128183.jpg" name="112" :score="7.3"></Book>
+    <Book pic="https://img3.doubanio.com/lpic/s23128183.jpg" name="杀死一只知更鸟" :score="9.9"></Book>
   </div>
 </template>
 
 <script>
 import Topbar from '@/components/Topbar'
+import Book from '@/components/Book'
 
 export default {
   components: {
-    Topbar
+    Topbar,
+    Book
   }
 }
 </script>
